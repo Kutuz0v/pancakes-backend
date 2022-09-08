b25d4102c5b9ad5faa1ef9630154c123ce9f952f
Kutuzov <101kutuzov@gmail.com>
2022-09-08 16:31:35 +0300

    Added pancakes verification on correct values

--- src/main/java/ua/pancakes/pancakesbackend/service/impl/PancakeServiceImpl.java
+++ src/main/java/ua/pancakes/pancakesbackend/service/impl/PancakeServiceImpl.java
@@ -3,6 +3,7 @@ package ua.pancakes.pancakesbackend.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import ua.pancakes.pancakesbackend.model.Ingredient;
 import ua.pancakes.pancakesbackend.model.Pancake;
 import ua.pancakes.pancakesbackend.repository.PancakeRepository;
 import ua.pancakes.pancakesbackend.service.ClientService;
@@ -23,14 +24,24 @@ public class PancakeServiceImpl
     PancakeRepository repository;
 
     @Override
-    public Pancake create(Pancake entity) {
-        entity.setClient(
+    public Pancake create(Pancake pancake) {
+        Integer price = 0, weight = 0;
+        for (Ingredient ingredient : pancake.getIngredients()) {
+            price += ingredient.getPrice();
+            weight += ingredient.getWeight();
+        }
+        if (!pancake.getPrice().equals(price) ||
+                !pancake.getWeight().equals(weight) ||
+                pancake.getIngredients().size() > 5)
+            throw new IllegalArgumentException("Bad ingredients");
+
+        pancake.setClient(
                 clientService.get(
                         ((UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
                                 .getId()
                 )
         );
-        return super.create(entity);
+        return super.create(pancake);
     }
 
     @Override
